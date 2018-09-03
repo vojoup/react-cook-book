61f778e8dd4d4b1314f553cd1141c01925837d0e
Vojta Oupicky <voj.oup@gmail.com>
2018-09-03 22:20:18 +0200

    Modifications for heroku deployment
    Move UI to client folder

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use('/api/recipes', recipes);
 if (process.env.NODE_ENV === 'production') {
   // * Set static folder
   app.use(express.static('../build'));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 app.listen(PORT, () => {
