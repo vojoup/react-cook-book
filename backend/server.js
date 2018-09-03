612177f01b05db5b49a654fa9a1d34f01f4f4f5f
Vojta Oupicky <voj.oup@gmail.com>
2018-09-03 22:46:57 +0200

    Use clients static folder

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/recipes', recipes);
 // * Serve static assests if in prod mode
 if (process.env.NODE_ENV === 'production') {
   // * Set static folder
-  app.use(express.static('../build'));
+  app.use(express.static('../client/build'));
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
